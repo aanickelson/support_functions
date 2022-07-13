277d6459526a02124dfe365f40a31ca940146000
aanickelson <aanickelson@gmail.com>
2022-07-13 16:58:40 -0400

    Added function that appends new number to the end of a filename if it already exists

--- safe_file_save.py
+++ safe_file_save.py
@@ -7,6 +7,31 @@ import errno
 import numpy as np
 
 
+def do_not_overwrite(og_path, fname, ext, data, isnp=False):
+    """
+    Uses path, filename, and extension given. Appends a
+    :param og_path: desired filepath without file name
+    :param fname: name of file without extension
+    :param ext: extension
+    :param data: data to save
+    :param isnp: True if wanting to save something in numpy
+    :return:
+    """
+    saved = False
+    num = 0
+    path = os.path.join(og_path, "{}.{}".format(fname, ext))
+    while not saved and num < 10:
+        try:
+            write(path, data, isnp)
+            saved = True
+            break
+        except FileExistsError:
+            path = os.path.join(og_path, "{}({:02d}).{}".format(fname, num, ext))
+            num += 1
+
+    return saved
+
+
 def write(path, text, is_np=False):
     if os.path.isfile(path):
         raise FileExistsError
